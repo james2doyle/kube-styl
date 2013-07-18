abb2dbb91be1d9aadf1083ee3420e70f217229dc
james2doyle <james2doyle@gmail.com>
2013-07-18 16:31:26 -0400

    initial with busted dorework :(

---
+++ css/dorework.js
@@ -0,0 +1,31 @@
+/* globals require, __dirname */
+/**
+ * Module Dependencies
+ */
+
+var rework = require('rework');
+var fs = require('fs');
+var vars = require('rework-vars');
+var math = require('rework-math');
+var shade = require('rework-shade');
+var css = fs.readFileSync(__dirname + '/style.temp.css', 'utf8');
+
+/**
+ * Parse
+ */
+
+ var res = rework(css)
+  // .use(vars())
+  // .use(math())
+  // .use(shade())
+  .use(clearfix())
+  .toString();
+
+/**
+ * Write file
+ */
+
+ fs.writeFile(__dirname + '/style.css', res, function (err) {
+  if (err) throw err;
+  console.log('saved');
+});
---
+++ gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['styl/ready.styl', 'css/style.css'],
+    concat: {
+      options: {
+        separator: '\n'
+      },
+      dist: {
+        src: ['styl/*.styl'],
+        dest: 'styl/ready.styl'
+      }
+    },
+    styl: {
+      options: {
+        // compress to remove comments
+        compress: true
+      },
+      dist: {
+        files: {
+          'css/style.temp.css': 'styl/ready.styl'
+        }
+      }
+    },
+    // grunt-rework needs a rework! So run a script instead
+    shell: {
+      doRework: {
+        command: 'node css/dorework.js'
+      }
+    },
+    uglify: {
+      main: {
+        files: {
+          'js/scripts.min.js': ['js/kube.buttons.js', 'js/kube.tabs.js']
+        }
+      }
+    },
+    growl: {
+      css: {
+        title: 'REWORK BUILT',
+        message: 'CSS files have been compiled.'
+      },
+      js: {
+        title: 'JAVASCRIPT BUILT',
+        message: 'JS files have been compiled.'
+      },
+      done: {
+        title: 'PROJECT BUILT',
+        message: 'all tasks have been completed.'
+      }
+    },
+    watch: {
+      styles: {
+        files: ['styl/*.css'],
+        tasks: ['clean', 'concat', 'styl', 'shell', 'growl:css'],
+        options: {
+          nospawn: true
+        },
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['uglify', 'growl:js'],
+        options: {
+          nospawn: true
+        },
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-growl');
+  grunt.loadNpmTasks('grunt-styl');
+
+  grunt.registerTask('default', ['clean', 'concat', 'styl', 'shell', 'uglify', 'growl:done']);
+  grunt.registerTask('css', ['clean', 'concat', 'styl', 'shell', 'growl:css']);
+  grunt.registerTask('js', ['uglify', 'growl:js']);
+};
